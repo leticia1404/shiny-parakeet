fc922967c12630dfe0c1eb2de52b6fead5c565ba
leticia1404 <let.demoura@gmail.com>
2019-06-26 15:17:42 -0300

    QuoteService

--- src/main/java/hello/QuoteController.java
+++ src/main/java/hello/QuoteController.java
@@ -1,13 +1,31 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class QuoteController {
 	
+	private static final Logger log = LoggerFactory.getLogger(QuoteController.class);
+	
+	private QuoteService qs;
+	
+	public QuoteController(RestTemplate restTemplate) {
+		qs = new QuoteService(restTemplate);
+	}
+
 	@GetMapping("/quotes")
-	public String showQuote() {
+	public String showQuote(RestTemplate restTemplate, Model model) {
+		QuoteService qs = new QuoteService(restTemplate);
+		Quote quote = qs.getQuote();
+        log.info(quote.toString());
+        model.addAttribute("q",quote);
 		return "quoteDetails";
 	}
+
+	
 }
